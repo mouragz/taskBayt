7b1c04d7ba8136721a308d982816cdfea840b4f3
Noura AlZboubi <nzoubi@cashU.local>
2018-01-17 09:59:30 +0200

    fix loading

--- src/app/components/layout.js
+++ src/app/components/layout.js
@@ -1,70 +1,95 @@
 import React from 'react';
 import $ from 'jquery';
+import { Doughnut, Bar, Line } from 'react-chartjs-2';
 
-import { Doughnut , Bar, Line} from 'react-chartjs-2';
+const componentsCharts = {
+  'Line': Line,
+  'Doughnut': Doughnut,
+  'Bar': Bar
+}
 
 class Layout extends React.Component {
+
   constructor() {
     super();
     this.state = {
-      charts: [],
-      types : ['Line','Doughnut','Bar']
+      priority: [],
+      charts: [null,null,null],
+      types: [],
+      loading: [false,false,false]
     }
   }
 
   componentDidMount() {
-    var _this = this;
-    function getData(name) {
-      return new Promise(function (resolve, reject) {
-        $.ajax({
-          url: "https://my-json-server.typicode.com/mouragz/chartsData/charts?type=" + name,
-          type: 'GET',
-          dataType: 'json',
-          error: function (err) {
-            console.log("error ", err);
-            reject(err);
-          },
-          success: function (data) {
-            console.log("success");
-            console.log(data);
-            delete data[0].type;
-            resolve(data);
-          }
-        });
+    var self = this;
+    function getCharts() {
+      $.ajax({
+        // please check all of them , each one have a certain priority ..
+        //url: "https://my-json-server.typicode.com/mouragz/priorityData/priority", // Line , Doughnut , Bar
+        //url: "https://my-json-server.typicode.com/mouragz/priorityData1/priority", // Bar , Doughnut , Line
+        url: "https://my-json-server.typicode.com/mouragz/priorityData2/priority", // Doughnut , Bar , Line 
+        type: 'GET',
+        dataType: 'json',
+        error: (err) => {
+          console.log("error ", err);
+        },
+        success: (data) => {
+          self.setState({ priority: data })
+          var names = Object.values(self.state.priority[0])
+          self.setState({ types: names })
+          var _self = self;
+          names.map((name, i) => {
+            console.info(_self.state.loading[i])
+            setTimeout(() => {
+              $.ajax({
+                url: "https://my-json-server.typicode.com/mouragz/chartsData/charts?type=" + name,
+                type: 'GET',
+                dataType: 'json',
+                error: (err) => {
+                  console.log("error ", err);
+                },
+                success: (data) => {
+                  console.log("success chart", data);
+                  delete data[0].type;
+                  let charts = _self.state.charts;
+                  let loading = _self.state.loading;
+                  charts[i]= data[0];
+                  loading[i]=true;
+                   _self.setState({
+                     charts: charts,
+                     loading: loading
+                   });
+                }
+              });
+            }, (i + 1) * 2 * 1000);
+          });
+        }
       });
     }
-    var printResult = (results) => {
-      console.log("Results = ", results);
-      _this.setState({ charts: results });
-    }
-
-    function main() {
-      Promise.all(
-        [getData('Line'), getData('Doughnut'), getData('Bar')]
-      ).then(printResult);
-    }
-    main();
+    getCharts();
   }
-  
+
   render() {
-    console.log("this is charts[0]"+JSON.stringify(this.state.charts[0]))
-    var arrCharts = [Line,Bar,Doughnut]
-    if(this.state.charts==null || this.state.charts == undefined || this.state.charts.length==0)
-    return null;
     return (
       <div>
         <div className="container">
-          <div className="chart-container">
-          {
-            this.state.types.map((type, i) =>
-            React.createElement(arrCharts[i],{
-                key:i,
-                data: this.state.charts[i][0]
-               }
-             ,null
-             )
-            )
-          }
+          <div className="chart-container" >
+            {
+             
+              this.state.charts.map((chart, i) =>(
+              this.state.loading[i] ?
+                  React.createElement(componentsCharts[this.state.types[i]],
+                    {
+                      key: i,
+                      data: this.state.charts[i],
+                     
+                    },
+                    null)
+                    : <h1 key={i}> loading ...</h1>
+                  )
+              )
+             
+            }
           </div>
         </div>
       </div>
